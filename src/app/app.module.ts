cff1c48cde345113bc0264f10634b3cdf1e31b6a
arivignesh <tulasigarivignesh1999@gmail.com>
2020-03-02 15:33:09 +0400

    ionic

---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { TranslateModule } from '@ngx-translate/core';
+import { RouterModule } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+
+import { environment } from '@env/environment';
+import { CoreModule } from '@app/core';
+import { SharedModule } from '@app/shared';
+import { HomeModule } from './home/home.module';
+import { ShellModule } from './shell/shell.module';
+import { AboutModule } from './about/about.module';
+import { LoginModule } from './login/login.module';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    ServiceWorkerModule.register('./ngsw-worker.js', {
+      enabled: environment.production
+    }),
+    FormsModule,
+    HttpClientModule,
+    TranslateModule.forRoot(),
+    IonicModule.forRoot(),
+    CoreModule,
+    SharedModule,
+    ShellModule,
+    HomeModule,
+    AboutModule,
+    LoginModule,
+    AppRoutingModule // must be imported as the last module as it contains the fallback route
+  ],
+  declarations: [AppComponent],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
